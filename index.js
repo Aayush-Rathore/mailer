f893697a57547fd0f5c3fe524c428a69e1a119ec
Aayush Rathore <aayu.r.2003@gmail.com>
2024-03-07 21:31:13 +0530

    🐛

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+require('./queue/mainQueues');
 const HTTP = require("http");
 
 HTTP.createServer(() => { console.log("Server is started!") });
 HTTP.get("/", (req, res) => {
     res.send("Your microservice is running!")
 })
-
-console.log("Server is running")
-require('./queue/mainQueues');
