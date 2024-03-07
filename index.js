b8d58f7c5d3d86020e79ed62b3241435c76a203d
Aayush Rathore <aayu.r.2003@gmail.com>
2024-03-07 21:30:35 +0530

    🐛

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const HTTP = require("http");
+
+HTTP.createServer(() => { console.log("Server is started!") });
+HTTP.get("/", (req, res) => {
+    res.send("Your microservice is running!")
+})
+
 console.log("Server is running")
 require('./queue/mainQueues');
