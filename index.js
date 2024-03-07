771e20ec890906c0f5b798a241aa8fd60400d0f7
Aayush Rathore <aayu.r.2003@gmail.com>
2024-03-07 21:38:34 +0530

    🐛 Http server created successfully

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 require('./queue/mainQueues');
-const HTTP = require("http");
 
-HTTP.createServer(() => { console.log("Server is started!") });
-HTTP.get("/", (req, res) => {
-    res.send("Your microservice is running!")
-})
+
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, world!\n');
+    }
+
+});
+
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+
+    console.log(`Server listening on port ${PORT}`);
+
+});
